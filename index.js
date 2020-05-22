d1aed565502ed093865722dc751cbcf6081945a2
CodyBrewer <codybrewerdev@gmail.com>
2020-05-22 15:50:56 -0600

    set up session

--- index.js
+++ index.js
@@ -2,20 +2,30 @@ require('dotenv').config();
 const express = require('express');
 const authRoutes = require('./routes/auth-routes');
 const passportSetup = require('./config/passport-setup');
+const passport = require('passport');
 const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
 
 const app = express();
-
+const PORT = process.env.PORT || 4000;
 
 // set up view engine
 app.set('view engine', 'ejs');
+
+// set up cookieSession
+app.use(cookieSession({
+  maxAge: 24 * 60 * 60 * 1000,
+  keys: [process.env.SECRET],
+}));
+
+// initialize passport
+app.use(passport.initialize());
+app.use(passport.session());
 
 // connect to mongodb
 mongoose.connect(process.env.MONGODB_URI, () => {
   console.log('connected to mongodb');
 });
-
-const PORT = process.env.PORT || 4000;
 
 // set up routes
 app.use('/auth', authRoutes);
