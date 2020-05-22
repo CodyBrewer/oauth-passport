32e5bc6348baa6bbb911e3de0b5d9b60360e58fa
CodyBrewer <codybrewerdev@gmail.com>
2020-05-22 13:55:30 -0600

    set up auth-routes

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const authRoutes = require('./routes/auth-routes');
 
 const app = express();
 
@@ -7,10 +8,13 @@ app.set('view engine', 'ejs');
 
 const PORT = process.env.PORT || 4000;
 
+// set up routes
+app.use('/auth', authRoutes);
+
 app.get('/', (req, res) => {
   res.render("home");
-})
+});
 
 app.listen(PORT, () => {
   console.log(`server listening on ${PORT}`);
-})
+});
