b3da38cbd130aa9c3b467068d213d265a9cb5a2a
CodyBrewer <codybrewerdev@gmail.com>
2020-05-22 14:39:18 -0600

    passport authenticate on /auth/google route

--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const passport = require('passport');
 
 // auth login
 router.get('/login', (req, res) => {
@@ -12,10 +13,9 @@ router.get('/logout', (req, res) => {
 })
 
 // auth with google
-router.get('/google', (req, res) => {
-  // handle with passport
-  res.send('logging in with google');
-});
+router.get('/google', passport.authenticate('google', {
+  scope: ['profile'],
+}));
 
 
 module.exports = router;
